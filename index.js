16e3164a9126f4294caaaa67ce5ebd8b6a29f014
Anthony Basco <abasco2@outlook.com>
2024-10-23 09:08:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const freelancers = [
+  { name: "Alice", price: 30, occupation: "writer" },
+  { name: "Bob", price: 50, occupation: "teacher" },
+  { name: "Carol", price: 50, occupation: "programmer" },
+];
+
